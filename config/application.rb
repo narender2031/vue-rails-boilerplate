7112e596cc0065ded27ee9f0d4c60c387f3d01ca
Narender Singh <narender2031@gmail.com>
2020-02-12 23:56:33 +0530

    Add PostgreSQL

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module TodoRails
+module VueRailsBoilerplate
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
