5c4cf930c01ba5388c08de6112b8c07d74d699e4
Narender Singh <narender2031@gmail.com>
2020-02-23 17:24:48 +0530

    Configure application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,27 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :set_session_locale
+  before_action :set_locale
+
+  private
+
+  def set_locale
+    I18n.locale = user_locale ||
+                  session[:locale] ||
+                  I18n.default_locale
+  end
+
+  def user_locale
+    locale = current_user&.language&.downcase&.to_sym
+    return locale if I18n.available_locales.include?(locale)
+  end
+
+  def set_session_locale
+    locale_from_params = params[:locale]&.to_sym
+    return unless I18n.available_locales.include?(locale_from_params)
+
+    session[:locale] = locale_from_params
+  end
 end
