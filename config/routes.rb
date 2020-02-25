ff22aaf1bbf825353f295790034cba7c415d1865
Narender Singh <narender2031@gmail.com>
2020-02-26 00:15:31 +0530

    device and mailcatcher setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, skip: [:sessions], controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+
+  as :user do
+    get 'login', to: 'users/sessions#new', as: :new_user_session
+    delete 'logout', to: 'devise/sessions#destroy', as: :user_session
+  end
+
   get 'hello/say_hello'
+  root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
